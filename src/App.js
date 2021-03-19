54bad5ff082b39999691bc7bc1e069b0ec034a7d
Clu <retsulcconnor@gmail.com>
2021-03-18 23:54:30 -0400

    set up navigation and made About page the home page. Added background image.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import About from './components/About';
+import Nav  from './components/Nav';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume'
+// import background from './assets/images/portfolio-background-large-min.jpg';
 import './App.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    switch(currentPage) {
+      case 'Portfolio':
+        return <Portfolio />;
+        case 'Contact':
+        return <Contact />;
+      case 'Resume':
+        return <Resume />;
+      default:
+        return <About />
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Nav currentPage={currentPage} setCurrentPage={setCurrentPage}></Nav>
+      <main>
+        {renderPage(currentPage)}
+      </main>
     </div>
   );
 }
 
 export default App;
+
+
+// style={{ backgroundImage: `url(${background})` }}
+// className='main-div'
+// role='img'
+// aria-label="mountainscape"
---
+++ src/components/Nav/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Nav(props) {
+    const navLinks = ['About', 'Portfolio', 'Contact', 'Resume'];
+    return(
+        <header>
+            <nav>
+                <ul className='flex-row'>
+                 {navLinks.map(tabs => (
+                     <li className='nav-link' key={tabs}>
+                         <a href={'#' + tabs.toLowerCase()}
+                         onClick={() => props.setCurrentPage(tabs)}
+                         className={
+                             props.currentPage === tabs ? 'active-nav' : 'nav-link'
+                         }
+                         >
+                            {tabs}
+                         </a>
+                     </li>
+                 ))}
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav;
