beaac02e9ed12758698022b7378533bb33c0e755
Clu <retsulcconnor@gmail.com>
2021-03-19 01:08:21 -0400

    added more styling to About page, including main content area.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import './About.css';
 
 function About() {
     return(
         <section>
-            <h1>About Me</h1>
+            <div className='about-me'>
+            <h1 className='about-title'>Who Am I?</h1>
+            <p className='about-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est aperiam eaque inventore debitis veniam ea natus autem sed quidem unde commodi quis nulla, vero quia, expedita laboriosam sint? Commodi, eum?</p>
+            </div>
         </section>
     )
 }
