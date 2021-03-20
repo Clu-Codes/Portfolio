629a083c2735f7706ea2408df261d8993a807797
Clu <retsulcconnor@gmail.com>
2021-03-20 13:33:45 -0400

    added picture

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './About.css';
+import CluCodes from '../../assets/Picture/clu-Codes.jpg';
 
 function About() {
     return(
         <section>
             <div className='about-me'>
             <h1 className='about-title'>Who Am I?</h1>
+            <img 
+            src={CluCodes} 
+            alt='photo of developer'
+            className='clu-img'
+            />
             <p className='about-text'>I'm Connor Cluster, a Florida-based full-stack web developer. I'm here to help you navigate the ever-evolving digital ecosystem to empower your business! I'll also share cute pictures of my beloved dog, Sydney. </p>
             </div>
         </section>
